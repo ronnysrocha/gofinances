b88ae7e05af88f7da8a31542d11f3ed1f85054bb
Ronny Rocha <ronnysrocha@gmail.com>
2022-03-22 19:32:22 -0300

    Salvando novas transações

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import {
   Modal,
   Keyboard,
@@ -10,7 +10,8 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 import { useForm } from 'react-hook-form'
-
+import uuid from 'react-native-uuid'
+import { useNavigation } from '@react-navigation/native';
 import { Button } from '../../components/Form/Button';
 import { InputForm } from '../../components/Form/InputForm';
 import { TransactionTypeButton } from '../../components/Form/TransactionTypeButton';
@@ -41,6 +42,10 @@ const schema = Yup.object().shape({
   .required('O valor é obrigatorio')
 })
 
+type NavigationProps = {
+  navigate:(screen:string) => void;
+}
+
 export function Register() {
   const [transactionType, setTransactionType] = useState('');
   const [categoryModalOpen, setCategoryModalOpen] = useState(false);
@@ -50,9 +55,12 @@ export function Register() {
     name: 'Categoria'
   });
 
+  const navigation = useNavigation<NavigationProps>();
+
   const {
     control,
     handleSubmit,
+    reset,
     formState: { errors }
   } = useForm({
     resolver: yupResolver(schema)
@@ -77,30 +85,40 @@ export function Register() {
     if (category.key === 'category')
       return Alert.alert('Selecione a categoria');
     
-    const data = {
+    const newTransaction = {
+      id: String(uuid.v4()),
       name: form.name,
       amount: form.amount,
       transactionType,
-      category: category.key
+      category: category.key,
+      date: new Date()
     }
 
-    try {      
-      await AsyncStorage.setItem(dataKey, JSON.stringify(data));
+    try { 
+      const data = await AsyncStorage.getItem(dataKey);
+      const currentData = data ? JSON.parse(data) : [];
+      
+      const dataFormatted = [
+        ...currentData,
+        newTransaction
+      ];
+
+      await AsyncStorage.setItem(dataKey, JSON.stringify(dataFormatted));
+
+      reset();
+      setTransactionType('');
+      setCategory({
+        key: 'category',
+        name: 'Categoria'
+      });
+
+      navigation.navigate('Listagem');
 
     } catch (error) {
       console.log(error);
       Alert.alert("Não foi possível salvar");
     }
   }
-
-  useEffect(() => {
-    async function loadData(){
-      const data = await AsyncStorage.getItem(dataKey);
-      console.log(JSON.parse(data!));
-    }
-
-    loadData();
-  }, [])
 
   return (
     <TouchableWithoutFeedback
