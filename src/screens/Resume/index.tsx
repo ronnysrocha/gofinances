e360ffa407d37f5f35dd7f2fed486f09561f3593
Ronny Rocha <ronnysrocha@gmail.com>
2022-03-29 23:00:26 -0300

    Estilização do filtro por mês

--- src/screens/Resume/index.tsx
+++ src/screens/Resume/index.tsx
@@ -4,13 +4,18 @@ import { VictoryPie } from 'victory-native';
 import { RFValue } from "react-native-responsive-fontsize";
 
 import { useTheme } from 'styled-components';
+import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs'
 import { HistoryCard } from "../../components/HistoryCard";
 import {
   Container,
   Header,
   Title,
   Content,
-  ChartContainer
+  ChartContainer,
+  MonthSelect,
+  MonthSelectButton,
+  MonthSelectIcon,
+  Month,
 } from './styles';
 import { categories } from "../../utils/categories";
 
@@ -90,7 +95,26 @@ export function Resume() {
         <Title>Resumo por categoria</Title>
       </Header>
 
-      <Content>
+      <Content
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{
+          paddingHorizontal: 24,
+          paddingBottom: useBottomTabBarHeight()
+        }}
+      >
+
+        <MonthSelect>
+          <MonthSelectButton>
+            <MonthSelectIcon name="chevron-left"/>
+          </MonthSelectButton>
+
+          <Month>Março</Month>
+
+          <MonthSelectButton>
+            <MonthSelectIcon name="chevron-right"/>
+          </MonthSelectButton>
+        </MonthSelect>
+
         <ChartContainer>
           <VictoryPie
             data={totalByCategories}
